eba6c4f2e63752e44655c704a223fe557a1be812
JahdonFaulcon <149533957+JahdonFaulcon@users.noreply.github.com>
2025-04-15 22:55:38 -0700

    Update SimplexNoise.java
    SimplexNoise.java commenting

--- Basic/src/main/java/SimplexNoise.java
+++ Basic/src/main/java/SimplexNoise.java
@@ -15,27 +15,27 @@ import java.util.Random;
 
 public class SimplexNoise {
 
-    SimplexNoise_octave[] octaves;
-    double[] frequencys;
-    double[] amplitudes;
-    int largestFeature;
-    double persistence;
-    int seed;
+    SimplexNoise_octave[] octaves; //array stores individual noise layers
+    double[] frequencys; //array stores frequency for each octave
+    double[] amplitudes; //arrays stores amplitudes for each octave
+    int largestFeature; //largest noise feature size
+    double persistence; //octave influence decay (0 - 1)
+    int seed; // seed for deterministic randomness
 
-    public SimplexNoise(int largestFeature, double persistence, int seed) {
+    public SimplexNoise(int largestFeature, double persistence, int seed) {//constructor for multi-octave simplex noise
         this.largestFeature = largestFeature;
         this.persistence = persistence;
         this.seed = seed;
-//recieves a number (eg 128) and calculates what power of 2 it is (eg 2^7)
+//recieves a number (eg 128) and calculates what power of 2 it is (eg 2^7); calculates octaves abased on largest feature
         int numberOfOctaves = (int) Math.ceil(Math.log10(largestFeature) / Math.log10(2));
         octaves = new SimplexNoise_octave[numberOfOctaves];
         frequencys = new double[numberOfOctaves];
         amplitudes = new double[numberOfOctaves];
         Random rnd = new Random(seed);
         for (int i = 0; i < numberOfOctaves; i++) {
-            octaves[i] = new SimplexNoise_octave(rnd.nextInt());
-            frequencys[i] = Math.pow(2, i);
-            amplitudes[i] = Math.pow(persistence, octaves.length - i);
+            octaves[i] = new SimplexNoise_octave(rnd.nextInt()); //creates octave with unique seed
+            frequencys[i] = Math.pow(2, i); //sets frequency for iteration's octave
+            amplitudes[i] = Math.pow(persistence, octaves.length - i); //sets amplitude of iteration's octave
         }
     }
 
@@ -50,7 +50,7 @@ public class SimplexNoise {
         return result;
     }
 
-    public double getNoise(int x, int y, int z) {
+    public double getNoise(int x, int y, int z) { //generates 3d simplex noise by combining multiple octaves
         double result = 0;
         for (int i = 0; i < octaves.length; i++) {
             double frequency = Math.pow(2, i);
