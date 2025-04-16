3660b959acfd51872bfd8e88c8d1aadc16a3f53a
Logan Bailey <98549940+Monster21Man@users.noreply.github.com>
2025-04-15 22:39:31 -0700

    Update SimplexNoise.java

--- Basic/Basic/src/main/java/SimplexNoise.java
+++ Basic/Basic/src/main/java/SimplexNoise.java
@@ -12,7 +12,7 @@ package main.java;
  * our Minecraft world so that mountains and valleys look more natural.
  ****************************************************************/ 
 import java.util.Random;
-
+//The overall SimplexNoise class
 public class SimplexNoise {
 
     SimplexNoise_octave[] octaves;
@@ -21,12 +21,14 @@ public class SimplexNoise {
     int largestFeature;
     double persistence;
     int seed;
-
+    //method: SimplexNoise
+    //purpose: This method creates the SimplexNoise constructer and assigns all its variables 
     public SimplexNoise(int largestFeature, double persistence, int seed) {
         this.largestFeature = largestFeature;
         this.persistence = persistence;
         this.seed = seed;
-//recieves a number (eg 128) and calculates what power of 2 it is (eg 2^7)
+        
+        //recieves a number (eg 128) and calculates what power of 2 it is (eg 2^7)
         int numberOfOctaves = (int) Math.ceil(Math.log10(largestFeature) / Math.log10(2));
         octaves = new SimplexNoise_octave[numberOfOctaves];
         frequencys = new double[numberOfOctaves];
@@ -39,17 +41,21 @@ public class SimplexNoise {
         }
     }
 
+    //method: getNoise
+    //purpose: This double method gets the noise level in a 2D plane
     public double getNoise(int x, int y) {
         double result = 0;
         for (int i = 0; i < octaves.length; i++) {
-//double frequency = Math.pow(2,i);
-//double amplitude = Math.pow(persistence,octaves.length-i);
+        //double frequency = Math.pow(2,i);
+        //double amplitude = Math.pow(persistence,octaves.length-i);
             result = result + octaves[i].noise(x / frequencys[i], y / frequencys[i])
                     * amplitudes[i];
         }
         return result;
     }
 
+    //method: getNoise
+    //purpose: This method gets the noise level in a 3D space
     public double getNoise(int x, int y, int z) {
         double result = 0;
         for (int i = 0; i < octaves.length; i++) {
